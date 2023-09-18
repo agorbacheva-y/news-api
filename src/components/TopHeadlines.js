56ad94e85fe6c73d9fe798cbb4f5272077ec2026
agorbacheva-y <agorbacheva.y@gmail.com>
2023-09-18 14:44:03 +0200

    show five top headlines

--- src/components/TopHeadlines.js
+++ src/components/TopHeadlines.js
@@ -7,18 +7,18 @@ const API_ENDPOINT = `https://newsapi.org/v2/top-headlines?language=en&apiKey=${
 const TopHeadlines = () => {
   const [ headlines, setHeadlines ] = useState({});
   const [ articles, setArticles ] = useState([]);
-  const [ topFiveArticles, setTopFiveArticles ] = useState([]);
 
   useEffect(() => {
     fetchNews();
   },[]);
   //console.log(headlines);
 
-  // useEffect(() => {
-  //   fetchHeadlines();
-  // },[headlines]);
-  // console.log(articles);
+  useEffect(() => {
+    fetchHeadlines();
+  },[headlines]);
+  console.log(articles);
 
+  // get data from newsapi
   const fetchNews = () => {
     let config = {'x-api-key': process.env.REACT_APP_NEWSAPI_KEY};
     axios.get(API_ENDPOINT, {
@@ -35,45 +35,24 @@ const TopHeadlines = () => {
     });
   };
 
-  // const fetchHeadlines = () => {
-  //   setArticles(headlines?.articles);
-  // };
-
-  // useEffect(() => {
-  //   const slicedArticles = articles.slice(0, 5);
-  //   const result = slicedArticles?.map(a => a.title);
-  //   setTopFiveArticles(result);
-  // },[articles]);
-
-  // console.log(topFiveArticles);
+  // save only the articles
+  const fetchHeadlines = () => {
+    const fiveHeadlines = headlines?.articles?.slice(0, 5);
+    setArticles(fiveHeadlines);
+  };
 
   return (
     <>
       <h1>Top Headlines</h1>
 
-      {/* <div>
-        {topFiveArticles?.map((item, i) =>
-        (
+      <div>
+        {articles?.map((item, i) => (
           <div key={i}>
-            <h3>{item}</h3>
+            <h3>{item.title}</h3>
           </div>
         ))}
-      </div> */}
-{/* 
-      <div>
-        <h3>{articles[0].title}</h3>
-        <p>{articles[0].source.name}</p>
       </div>
 
-      <div>
-        <h3>{articles[1].title}</h3>
-        <p>{articles[1].source.name}</p>
-      </div>
-      
-      <div>
-        <h3>{articles[2].title}</h3>
-        <p>{articles[2].source.name}</p>
-      </div> */}
     </>
   );
 };
