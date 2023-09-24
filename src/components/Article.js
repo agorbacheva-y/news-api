dfe14f5f95190ed2f57f36a1c9caf45f7ffa7ce5
agorbacheva-y <agorbacheva.y@gmail.com>
2023-09-24 22:20:22 +0200

    useNavigate on back tab Article.js

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,14 +1,15 @@
-import {useLocation} from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import Paper from "@mui/material/Paper";
 import Tab from "@mui/material/Tab";
 import TabList from '@mui/lab/TabList';
 import TabContext from '@mui/lab/TabContext';
 
 function Article() {
+  const navigate = useNavigate();
   const location = useLocation();
 
-  const title = location.state.usArticles.title;
-  const url = location.state.usArticles.url;
+  // const title = location.state.usArticles.title;
+  // const url = location.state.usArticles.url;
 
 
   return (
@@ -19,12 +20,12 @@ function Article() {
             textColor="primary"
             indicatorColor="primary"
           >
-            <Tab label="Back"/>
-            <Tab label={title} />
+            <Tab label="Back" onClick={() => navigate(-1) } />
+            {/* <Tab label={title} /> */}
           </TabList>
         </Paper>
       </TabContext>
-      {url}
+      {/* {url} */}
     </div>
   );
 };
