515da76303affd6c9286ea022601571069794a39
agorbacheva-y <agorbacheva.y@gmail.com>
2023-09-24 22:35:36 +0200

    add value to Tab in Article.js

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,13 @@
 import { useLocation, useNavigate } from 'react-router-dom';
+import { useState } from "react";
 import Paper from "@mui/material/Paper";
 import Tab from "@mui/material/Tab";
 import TabList from '@mui/lab/TabList';
 import TabContext from '@mui/lab/TabContext';
 
 function Article() {
+  const [ value, setValue ] = useState("1"); // change to 2 after fix url link
+
   const navigate = useNavigate();
   const location = useLocation();
 
@@ -14,14 +17,14 @@ function Article() {
 
   return (
     <div>
-      <TabContext>
+      <TabContext value={value} >
         <Paper square>
           <TabList
             textColor="primary"
             indicatorColor="primary"
           >
-            <Tab label="Back" onClick={() => navigate(-1) } />
-            {/* <Tab label={title} /> */}
+            <Tab label="Back" value="1" onClick={() => navigate(-1) } />
+            {/* <Tab label={title} value="2" /> */}
           </TabList>
         </Paper>
       </TabContext>
